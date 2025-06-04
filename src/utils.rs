1821e619f71099c5c4f12f25340594fe0f010041
Andeskjerf <predgd@pm.me>
2025-06-04 23:10:29 +0200

    remove old functions

--- src/utils.rs
+++ src/utils.rs
@@ -1,22 +1,3 @@
-pub fn print_vec(input: &Vec<String>) {
-    for elem in input {
-        println!("{elem}");
-    }
-}
-
-pub fn strip_until_backwards(remove_until: char, input: &str) -> String {
-    let mut to_strip = String::new();
-    input.clone_into(&mut to_strip);
-    let mut stripped = String::new();
-    while let Some(c) = to_strip.pop() {
-        if c == remove_until {
-            break;
-        }
-        stripped.push(c);
-    }
-    stripped.chars().rev().collect()
-}
-
 pub fn strip_until_match(remove_until: &str, input: &str) -> String {
     let mut to_strip = String::new();
     input.clone_into(&mut to_strip);
