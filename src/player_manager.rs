f0f7d9b0c26c4472fc80ba007e8bbf3e37270308
Andeskjerf <predgd@pm.me>
2025-06-22 15:58:26 +0200

    add listener threads for player manager

--- src/player_manager.rs
+++ src/player_manager.rs
@@ -7,6 +7,7 @@ use crate::{
     player_client::PlayerClient,
 };
 use std::{
+    collections::HashMap,
     sync::{Arc, Mutex},
     thread::{self, JoinHandle},
 };
@@ -25,19 +26,41 @@ so: PlayerManager needs to do a few things.
 */
 
 pub struct PlayerManager {
-    players: Arc<Mutex<Vec<PlayerClient>>>,
+    players: Arc<Mutex<HashMap<String, PlayerClient>>>,
     event_bus: Arc<Mutex<EventBus>>,
 }
 
 impl PlayerManager {
     pub fn new(event_bus: Arc<Mutex<EventBus>>) -> Self {
         Self {
-            players: Arc::new(Mutex::new(vec![])),
+            players: Arc::new(Mutex::new(HashMap::new())),
             event_bus,
         }
     }
 
-    fn listen_event_bus(&self) {
+    fn init_listener_threads(self: Arc<Self>) {
+        {
+            let player_manager = Arc::clone(&self);
+            thread::spawn(move || player_manager.listen_playback_changed());
+        }
+        {
+            let player_manager = Arc::clone(&self);
+            let players = Arc::clone(&player_manager.players);
+            thread::spawn(move || player_manager.listen_song_changed(players));
+        }
+    }
+
+    fn listen_playback_changed(self: Arc<Self>) {
+        let rx = self
+            .event_bus
+            .lock()
+            .unwrap()
+            .subscribe(EventType::PlaybackChanged);
+
+        loop {}
+    }
+
+    fn listen_song_changed(&self, players: Arc<Mutex<HashMap<String, PlayerClient>>>) {
         let rx = self
             .event_bus
             .lock()
@@ -46,7 +69,7 @@ impl PlayerManager {
 
         loop {
             let msg = rx.recv();
-            let metadata: (MprisMetadata, usize) = match msg {
+            let (metadata, len): (MprisMetadata, usize) = match msg {
                 Ok(encoded) => {
                     bincode::decode_from_slice(&encoded[..], config::standard()).unwrap()
                 }
@@ -55,6 +78,19 @@ impl PlayerManager {
                     continue;
                 }
             };
+
+            let mut lock = players.lock().unwrap();
+            let player = lock.get_mut(&metadata.player_id);
+
+            match player {
+                Some(player) => player.update_metadata(metadata),
+                None => {
+                    players.lock().unwrap().insert(
+                        metadata.player_id.clone(),
+                        PlayerClient::new(&metadata.player_id.clone(), metadata),
+                    );
+                }
+            }
         }
     }
 }
@@ -62,7 +98,7 @@ impl PlayerManager {
 impl Runnable for PlayerManager {
     fn run(self: Arc<Self>) -> JoinHandle<()> {
         thread::spawn(move || {
-            self.listen_event_bus();
+            self.init_listener_threads();
         })
     }
 }
