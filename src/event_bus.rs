728b89fbb572d24abd518eab78655cbbf2950745
Andeskjerf <predgd@pm.me>
2025-06-22 14:53:14 +0200

    add event bus

---
+++ src/event_bus.rs
@@ -0,0 +1,32 @@
+use std::{collections::HashMap, sync::mpsc};
+
+#[derive(Eq, Hash, PartialEq)]
+pub enum EventType {
+    PlayerSongChanged,
+}
+
+pub struct EventBus {
+    senders: HashMap<EventType, Vec<mpsc::Sender<Vec<u8>>>>,
+}
+
+impl EventBus {
+    pub fn new() -> Self {
+        Self {
+            senders: HashMap::new(),
+        }
+    }
+
+    pub fn subscribe(&mut self, event_type: EventType) -> mpsc::Receiver<Vec<u8>> {
+        let (tx, rx) = mpsc::channel();
+        self.senders.entry(event_type).or_default().push(tx);
+        rx
+    }
+
+    pub fn publish(&self, event_type: EventType, event: Vec<u8>) {
+        if let Some(senders) = self.senders.get(&event_type) {
+            for sender in senders {
+                let _ = sender.send(event.clone());
+            }
+        }
+    }
+}
