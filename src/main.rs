73201e05ee7d20b10e4703c7b14c2e4829165e67
Andeskjerf <predgd@pm.me>
2025-06-26 16:16:17 +0200

    add logging deps & init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::{sync::Arc, thread};
+use std::{
+    fs::{self, File},
+    sync::Arc,
+    thread,
+};
 
 use event_bus::EventBus;
 use interfaces::dbus_client::DBusClient;
 use services::{
     dbus_monitor::DBusMonitor, display::Display, player_manager::PlayerManager, runnable::Runnable,
 };
+use simplelog::{CombinedLogger, Config, WriteLogger};
 
 mod effects;
 mod event_bus;
@@ -17,6 +22,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: arg handling with clap
     // TODO: events, like sending signal to play/pause active player
     // TODO: logging
+    let cache = dirs::cache_dir().ok_or_else(|| {
+        std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "could not get user's cache directory",
+        )
+    })?;
+
+    let app_cache_dir = cache.join("waybar-module-music");
+    fs::create_dir(&app_cache_dir)?;
+
+    let log_path = app_cache_dir.join("app.log");
+
+    CombinedLogger::init(vec![WriteLogger::new(
+        log::LevelFilter::Debug,
+        Config::default(),
+        File::create(log_path)?,
+    )])?;
 
     let (event_bus, event_bus_handle) = EventBus::new();
     thread::spawn(move || {
@@ -25,7 +47,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let dbus_client = Arc::new(DBusClient::new());
 
-    let actors: Vec<Arc<dyn Runnable>> = vec![
+    let services: Vec<Arc<dyn Runnable>> = vec![
         Arc::new(DBusMonitor::new(
             event_bus_handle.clone(),
             dbus_client.clone(),
@@ -38,7 +60,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ];
 
     let mut handles = vec![];
-    for actor in actors {
+    for actor in services {
         handles.push(actor.run());
     }
 
