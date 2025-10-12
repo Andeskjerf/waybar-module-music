92fa5c1bffcd68654a1204260550ba7e9ff4303b
Andeskjerf <predgd@pm.me>
2025-10-12 10:45:54 +0200

    parse and send seeked events

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,5 +2,6 @@ pub mod args;
 pub mod config;
 pub mod mpris_metadata;
 pub mod mpris_playback;
+pub mod mpris_seeked;
 pub mod player_client;
 pub mod player_state;
---
+++ src/models/mpris_seeked.rs
@@ -0,0 +1,30 @@
+use bincode::{Decode, Encode};
+use dbus::Message;
+use log::error;
+
+#[derive(Debug, Default, Clone, Encode, Decode, PartialEq)]
+pub struct MprisSeeked {
+    pub player_id: String,
+    pub timestamp: i64,
+}
+
+impl MprisSeeked {
+    pub fn new(player_id: String) -> Self {
+        Self {
+            player_id,
+            timestamp: 0,
+        }
+    }
+
+    pub fn from_dbus_message(msg: &Message) -> Self {
+        let mut result = MprisSeeked::new(msg.sender().unwrap().to_string());
+
+        if let Some(position) = msg.get1::<i64>() {
+            result.timestamp = position;
+            return result;
+        }
+
+        error!("got to end of MprisSeeked constructor without returning during construction, this should not happen");
+        result
+    }
+}
